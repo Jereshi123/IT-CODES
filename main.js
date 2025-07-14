1e965d4bdb2698f78b99771653cce497e913e030
Jereshi123 <pozonjeromeandre@gmail.com>
2025-07-14 21:50:52 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log('Hello World!')
+
+//Kelvin to Celsius
+console.log(293 - 273)
+        
+//Celsius to Fahrenheit
+console.log(20 * 9 / 5 + 32)
+
+const Fahrenheit = '68'
+console.log(Fahrenheit);
+console.log('The temperature is' ${Fahrenheit}'degrees fahrenheit')
